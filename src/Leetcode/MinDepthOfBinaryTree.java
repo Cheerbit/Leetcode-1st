e4396ecdbdba0219ebbd159339455e694cae96d5
Wenqi Zhu <loopcoder@Wenqis-MacBook-Pro.local>
2014-12-19 16:26:23 -0800

    12/19

--- src/Leetcode/MinDepthOfBinaryTree.java
+++ src/Leetcode/MinDepthOfBinaryTree.java
@@ -28,10 +28,8 @@ public class MinDepthOfBinaryTree {
 			return Math.min(minDepth(root.left), minDepth(root.right)) +1;
 		}else if(root.left!=null){
 			return minDepth(root.left) +1;
-		}else if(root.right!=null){
+		}else {
 			return minDepth(root.right) +1;
-		}else{
-			return 1;
 		}
 	}
 }
---
+++ src/Leetcode/SymmetricTree.java
@@ -0,0 +1,36 @@
+package Leetcode;
+
+/**
+ * Created by 7 
+ * @Buffalo
+ * 7:16:48 PM
+ * Dec 19, 2014
+ * Given a binary tree, check whether it is a mirror of itself 
+ * (ie, symmetric around its center).
+
+For example, this binary tree is symmetric:
+
+    1
+   / \
+  2   2
+ / \ / \
+3  4 4  3
+But the following is not:
+    1
+   / \
+  2   2
+   \   \
+   3    3
+Note:
+Bonus points if you could solve it both recursively and iteratively.
+ */
+public class SymmetricTree {
+	private boolean isSymmetric(TreeNode left, TreeNode right){
+		if(left==null && right == null) return true;
+		if(left ==null || right ==null || left.val!=right.val) return false;
+		return isSymmetric(left.left, right.right) && isSymmetric(left.right, right.left);
+	}
+	public boolean isSymmetric(TreeNode root) {
+        return (root==null) || isSymmetric(root.left,root.right);
+    }
+}
